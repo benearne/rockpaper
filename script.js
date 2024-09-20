bbfab0052485bf02dc98be43e458bdb0a5700edf
benearne <b.arnold@posteo.de>
2024-09-20 21:17:22 +0200

    add game

---
+++ script.js
@@ -0,0 +1,58 @@
+function getComputerChoice() {
+    let rock = "rock"
+    let paper = "paper"
+    let sissors = "sissors"
+    
+    let num = Math.random()
+
+    if (num > 0.33 && num < 0.66)
+        return rock
+    else if (num > 0.66)
+        return paper
+    else
+        return sissors
+}
+
+function getHumanChoice() {
+    let user = prompt("type your choice: ")
+    return user;
+}
+
+var humanScore = 0
+var computerScore = 0
+
+function playGame() {
+    let rounds = 0
+
+    while (rounds < 5) {
+        var humanChoice = getHumanChoice();
+        var computerChoice = getComputerChoice();
+        
+        if (humanChoice == computerChoice) {
+        }
+        else if ((humanChoice == "rock" && computerChoice == "paper") 
+            || (humanChoice == "paper" && computerChoice == "sissors")
+            || (humanChoice == "sissors" && computerChoice == "rock")) {
+                computerScore ++;
+        } else {
+            humanScore ++;
+        }
+
+        rounds ++;
+    }
+
+}
+
+playGame()
+
+if (humanScore == computerScore) {
+    console.log("its a tie!")
+}
+else if (humanScore > computerScore) {
+    console.log("human wins!")
+}
+else {
+    console.log("computer wins!")
+}
+console.log("human: " + humanScore + 
+    ' vs. \ncomputer: ' + computerScore)
